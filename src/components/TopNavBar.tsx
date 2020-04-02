63ad32d1c58fda6c106fe2a03a28e5b075602ad3
Kim Seungha <seungha.me@gmail.com>
2020-04-02 23:09:40 +0900

    초기화

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -2,6 +2,7 @@ import React, { FC, ReactNode } from 'react'
 import { createPortal } from 'react-dom'
 import s from './TopNavBar.module.scss'
 import { Lace } from './Lace'
+import { usePersistency } from '../contexts/PersistencyContext'
 
 const topNavSlot = document.querySelector('#top-nav')!
 
@@ -10,16 +11,26 @@ export const TopNavBar: FC<{
   action?: ReactNode
   progress: number
 }> = ({ title, action, progress }) => {
+  const persistency = usePersistency()
+
   return createPortal(
     <div className={s.container}>
       <div className={s.progressBar} style={{ width: `${progress * 100}%` }} />
-      <div className={s.backButton}>{/*TODO*/}</div>
       <div className={s.title}>{title}</div>
+      <div
+        className={s.backButton}
+        onClick={() => {
+          if (window.confirm('처음으로 돌아가시겠어요?')) {
+            persistency.action.reset()
+          }
+        }}
+      />
       {action && (
         <div id="navAction" className={s.actionButton}>
           {action}
         </div>
       )}
+
       <div className={s.lace}>
         <Lace />
       </div>
