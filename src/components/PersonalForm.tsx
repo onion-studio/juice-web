0f1d5b527338f4c9b64aa51fbdb6c740449cc975
jeesoo <jeesoo.hong@gmail.com>
2020-04-04 14:10:16 +0900

    지역 순서 변경

--- src/components/PersonalForm.tsx
+++ src/components/PersonalForm.tsx
@@ -85,23 +85,23 @@ export const PersonalForm: FC<Props> = ({ onSubmit, onDismiss }) => {
           <label className={s.label}>거주 지역</label>
           <select className={s.select} ref={locationRef} required>
             <option disabled selected hidden />
-            <option value="서울특별시">서울특별시</option>
-            <option value="부산광역시">부산광역시</option>
-            <option value="대구광역시">대구광역시</option>
-            <option value="인천광역시">인천광역시</option>
-            <option value="광주광역시">광주광역시</option>
-            <option value="대전광역시">대전광역시</option>
-            <option value="울산광역시">울산광역시</option>
-            <option value="세종특별자치시">세종특별자치시</option>
-            <option value="경기도">경기도</option>
             <option value="강원도">강원도</option>
-            <option value="충청북도">충청북도</option>
-            <option value="충청남도">충청남도</option>
-            <option value="전라북도">전라북도</option>
-            <option value="전라남도">전라남도</option>
+            <option value="경기도">경기도</option>
+            <option value="광주광역시">광주광역시</option>
             <option value="경상북도">경상북도</option>
             <option value="경상남도">경상남도</option>
+            <option value="대구광역시">대구광역시</option>
+            <option value="대전광역시">대전광역시</option>
+            <option value="부산광역시">부산광역시</option>
+            <option value="서울특별시">서울특별시</option>
+            <option value="세종특별자치시">세종특별자치시</option>
+            <option value="인천광역시">인천광역시</option>
+            <option value="전라북도">전라북도</option>
+            <option value="전라남도">전라남도</option>
             <option value="제주특별자치도">제주특별자치도</option>
+            <option value="울산광역시">울산광역시</option>
+            <option value="충청북도">충청북도</option>
+            <option value="충청남도">충청남도</option>
             <option value="해외">해외</option>
           </select>
           <div className={s.placeholder}>거주 지역을 선택해 주세요</div>
