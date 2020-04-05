6818197593bce7c1a582778e2b13a675cfe21dab
jeesoo <jeesoo.hong@gmail.com>
2020-04-05 23:57:50 +0900

    credit 스타일링, 내용 추가

--- src/components/Credit.tsx
+++ src/components/Credit.tsx
@@ -1,135 +1,165 @@
 import React, { FC } from 'react'
 import s from './Credit.module.scss'
+import { TopNavBar } from './TopNavBar'
 
 export const Credit: FC = () => {
   return (
-    <div className={s.wrap}>
-      <div>“세상이 미워도 투표를 멀리하지 말자”</div>
-      <div className={s.card}>
-        <div className={s.col}>
-          <div className={s.item}>
-            <div className={s.name}>댄</div>
-            <div className={s.role}>Dev(client)</div>
+    <>
+      <TopNavBar title="공약쥬스를 만든 사람들" progress={0} isCredit={true} />
+      <div className={s.wrap}>
+        <div className={s.header}>
+          “세상이 미워도
+          <br />
+          투표를 멀리하지 말자”
+        </div>
+        <div className={s.title}>PACTUM</div>
+        <div className={`${s.card} ${s.pactum}`}>
+          <div></div>
+          <div className={s.col}>
+            <div className={s.item}>
+              <div className={s.name}>댄</div>
+              <div className={s.role}>Dev(client)</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>쌈</div>
+              <div className={s.role}>Contents</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>숨</div>
+              <div className={s.role}>Marketing</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>씨</div>
+              <div className={s.role}>UX</div>
+            </div>
           </div>
-          <div className={s.item}>
-            <div className={s.name}>쌈</div>
-            <div className={s.role}>Contents</div>
+          <div className={s.col}>
+            <div className={s.item}>
+              <div className={s.name}>윌리</div>
+              <div className={s.role}>Contents</div>
+            </div>
+            <div className={s.item}>
+              <a
+                href="https://www.instagram.com/eun9ook/"
+                className={s.name}
+                style={{ textDecoration: 'underline' }}
+              >
+                은국
+              </a>
+              <div className={s.role}>Design</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>솜</div>
+              <div className={s.role}>Marketing</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>키</div>
+              <div className={s.role}>Contents</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>퍼핀</div>
+              <div className={s.role}>Dev(server)</div>
+            </div>
           </div>
-          <div className={s.item}>
-            <div className={s.name}>숨</div>
-            <div className={s.role}>Marketing</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>씨</div>
-            <div className={s.role}>UX</div>
+          <a className={s.link} href="https://brunch.co.kr/@pactum">
+            <span>Pactum Blog</span>
+          </a>
+        </div>
+        <div className={s.title}>Partners</div>
+        <div className={`${s.card} ${s.partner}`}>
+          <div className={s.col}>
+            <div className={s.item}>
+              <div className={s.name}>nonce</div>
+              <a className={s.role} href="https://nonce.community/">
+                nonce.community
+              </a>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>NEWNEEK</div>
+              <a href="https://newneek.co/" className={s.role}>
+                newneek.co
+              </a>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>잼있는인생</div>
+              <a href="http://jaminlife.me/" className={s.role}>
+                jaminlife.me
+              </a>
+            </div>
           </div>
         </div>
-        <div className={s.col}>
-          <div className={s.item}>
-            <div className={s.name}>윌리</div>
-            <div className={s.role}>Contents</div>
+        <div className={s.title}>Advisors</div>
+        <div className={`${s.card} ${s.advisor}`}>
+          <div className={s.col}>
+            <div className={s.item}>
+              <div className={s.name}>국일</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>래현</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>예진</div>
+            </div>
           </div>
-          <div className={s.item}>
-            <div className={s.name}>은국</div>
-            <div className={s.role}>Design</div>
+          <div className={s.col}>
+            <div className={s.item}>
+              <div className={s.name}>예찬</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>신수</div>
+            </div>
           </div>
-          <div className={s.item}>
-            <div className={s.name}>솜</div>
-            <div className={s.role}>Marketing</div>
+          <a className={s.link} href="https://brunch.co.kr/@pactum/8">
+            <span>Advisor Story</span>
+          </a>
+        </div>
+        <div className={s.title}>Angel Inverstors</div>
+        <div className={`${s.card} ${s.investor}`}>
+          <div className={s.col}>
+            <div className={s.item}>
+              <div className={s.name}>강영세</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>김서진</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>김승덕</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>김영원</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>김주연</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>김형석</div>
+            </div>
           </div>
-          <div className={s.item}>
-            <div className={s.name}>키</div>
-            <div className={s.role}>Contents</div>
+          <div className={s.col}>
+            <div className={s.item}>
+              <div className={s.name}>박준호</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>이성은</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>이진경</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>차이새</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>송창석</div>
+            </div>
+            <div className={s.item}>
+              <div className={s.name}>김형석</div>
+            </div>
           </div>
-          <div className={s.item}>
-            <div className={s.name}>퍼핀</div>
-            <div className={s.role}>Dev(server)</div>
-          </div>
+          <a className={s.link} href="https://www.presenu.com/nonce/?idx=58">
+            <span>투자유치 이야기</span>
+          </a>
         </div>
       </div>
-      <div className={s.card}>
-        <div className={s.col}>
-          <div className={s.item}>
-            <div className={s.name}>nonce</div>
-            <div className={s.role}>nonce.community</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>NEWNEEK</div>
-            <div className={s.role}>newneek.co.kr</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>잼있는인생</div>
-            <div className={s.role}>jaminlife.me</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>씨</div>
-            <div className={s.role}>UX</div>
-          </div>
-        </div>
-      </div>
-      <div className={s.card}>
-        <div className={s.col}>
-          <div className={s.item}>
-            <div className={s.name}>국일</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>래현</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>예진</div>
-          </div>
-        </div>
-        <div className={s.col}>
-          <div className={s.item}>
-            <div className={s.name}>예찬</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>신수</div>
-          </div>
-        </div>
-      </div>
-      <div className={s.card}>
-        <div className={s.col}>
-          <div className={s.item}>
-            <div className={s.name}>강영세</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>김서진</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>김승덕</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>김영원</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>김주연</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>김형석</div>
-          </div>
-        </div>
-        <div className={s.col}>
-          <div className={s.item}>
-            <div className={s.name}>박준호</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>이성은</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>이진경</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>차이새</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>송창석</div>
-          </div>
-          <div className={s.item}>
-            <div className={s.name}>김형석</div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
